77e13f01f523eb928d4504259521bc327321614c
Crazy_Click <dima.samik.1506@gmail.com>
2022-06-21 20:30:44 +0500

    ebal

--- BeatySalon/BeatySalon/obj/Debug/net5.0-windows/Views/AuthorizationWindow.g.i.cs
+++ BeatySalon/BeatySalon/obj/Debug/net5.0-windows/Views/AuthorizationWindow.g.i.cs
@@ -62,7 +62,7 @@ namespace BeatySalon.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BeatySalon;V1.0.0.0;component/views/authorizationwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BeatySalon;component/views/authorizationwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\AuthorizationWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
