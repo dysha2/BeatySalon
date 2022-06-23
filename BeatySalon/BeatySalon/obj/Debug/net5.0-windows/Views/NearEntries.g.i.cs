f37358ccd5056f4ce3bee3fd0608197edbe91418
dysha2 <dyha035@gmail.com>
2022-06-23 14:48:52 +0300

    StartMegre

--- BeatySalon/BeatySalon/obj/Debug/net5.0-windows/Views/NearEntries.g.i.cs
+++ BeatySalon/BeatySalon/obj/Debug/net5.0-windows/Views/NearEntries.g.i.cs
@@ -54,7 +54,7 @@ namespace BeatySalon.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BeatySalon;component/views/nearentries.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BeatySalon;V1.0.0.0;component/views/nearentries.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\NearEntries.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
